5a49c8695dcea43eef04bd55518c9bb5dccade33
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2025-03-01 22:20:23 +0100

    Security merge

---
+++ desktop-plant/scripts/state machine/state_machine.gd
@@ -0,0 +1,37 @@
+extends Node
+
+@export var parent: Character
+@export var starting_state: State
+var current_state: State
+
+func init(parent: Character) -> void:
+	for child in get_children():
+		child.parent = parent
+	
+	change_state(starting_state)
+
+func change_state(new_state: State) -> void:
+	if current_state:
+		current_state.exit()
+
+	current_state = new_state
+	current_state.enter()
+	
+# Pass through functions for the Player to call,
+# handling state changes as needed.
+func process_physics(delta: float) -> void:
+	var new_state = current_state.process_physics(delta)
+	if new_state:
+		change_state(new_state)
+
+func process_input(event: InputEvent) -> void:
+	if !current_state:
+		return
+	var new_state = current_state.process_input(event)
+	if new_state:
+		change_state(new_state)
+
+func process_frame(delta: float) -> void:
+	var new_state = current_state.process_frame(delta)
+	if new_state:
+		change_state(new_state)
